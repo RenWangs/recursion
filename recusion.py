9fb58d64e14e98c83aa4a64c945174267259fd3b
reneecode <renwang.cn@gmail.com>
2020-04-11 17:10:58 +1200

    Initial commit

---
+++ recusion.py
@@ -0,0 +1,15 @@
+# 回文字符串
+def ispalindrome(s):
+    if len(s) <=1: return True
+    else: return s[0] == s[-1] and ispalindrome(s[1:-1])
+
+def ispalindrome1(s,indent):
+    print(indent,"ispalindrome1调用",s)
+    if len(s) <= 1:
+        print(indent,"准备从基础情况返回true")
+        return True
+    else:
+        ans = s[0] == s[-1] and ispalindrome1(s[1:-1],indent + indent)
+        print(indent, "准备返回",ans)
+
+print(ispalindrome("abccba"))
